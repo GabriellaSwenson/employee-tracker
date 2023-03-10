758b6d37eedfe2f69a3f4120a5a559941a8cecbf
GabriellaSwenson <unipuppydj@gmail.com>
2023-03-10 11:28:19 -0600

    Made tables

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES (IT),
+    (Accounting),
+    (Programming),
+    (Graphics),
+    (Sound Design);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("IT Specialist", 80000, 1),
+    ("Accountant", 80000, 2),
+    ("Physics Programmer", 90000, 3),
+    ("Graphic Designer", 50000, 4),
+    ("Sound Designer", 65000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Annie", "Smith", 2, null),
+    ("John", "Doe", 5, null),
+    ("Jack", "Anderson", 3, null),
+    ("Emily", "Jackson", 1, null),
+    ("Marting", "Gray", 4, null);
