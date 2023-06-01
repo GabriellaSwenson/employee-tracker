92dd8373b58edc234752e0fd2217d3feb773378a
GabriellaSwenson <unipuppydj@gmail.com>
2023-05-31 23:12:25 -0500

    promises

--- server.js
+++ server.js
@@ -34,14 +34,10 @@ function manageTeam() {
     })
     .then(async (response) => {
       switch (response.choices) {
-        new Promise((resolve, reject) => {
         case "View Departments":
           await viewDepartments();
           manageTeam();
           break;
-          resolve()
-          });
-
 
         case "Add Department":
           addDepartment();
@@ -69,163 +65,181 @@ function manageTeam() {
 }
 
 function viewDepartments() {
-  db.query(`SELECT id, department_name FROM departments`, (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    console.log(rows);
+  new Promise((resolve, reject) => {
+    db.query(`SELECT id, department_name FROM departments`, (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      console.log(rows);
+    });
+    resolve();
   });
 }
 
 function viewRoles() {
-  db.query(
-    `SELECT id, title, salary, department_id FROM roles`,
-    (err, rows) => {
-      if (err) {
-        throw err;
+  new Promise((resolve, reject) => {
+    db.query(
+      `SELECT id, title, salary, department_id FROM roles`,
+      (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        console.log(rows);
       }
-      console.log(rows);
-    }
-  );
+    );
+    resolve();
+  });
 }
 
 function viewEmployees() {
-  db.query(
-    `SELECT id, first_name, last_name, role_id, manager_id FROM employees`,
-    (err, rows) => {
-      if (err) {
-        throw err;
+  new Promise((resolve, reject) => {
+    db.query(
+      `SELECT id, first_name, last_name, role_id, manager_id FROM employees`,
+      (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        console.log(rows);
       }
-      console.log(rows);
-    }
-  );
+    );
+    resolve();
+  });
 }
 
 function addDepartment() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "addDepartment",
-        message: "What is the name of the department?",
-      },
-    ])
-    .then((answers) => {
-      db.query(
-        "INSERT INTO departments (department_name) VALUES (?)",
-        [answers.addDepartment],
-        (err, rows) => {
-          if (err) {
-            throw err;
+  new Promise((resolve, reject) => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "addDepartment",
+          message: "What is the name of the department?",
+        },
+      ])
+      .then((answers) => {
+        db.query(
+          "INSERT INTO departments (department_name) VALUES (?)",
+          [answers.addDepartment],
+          (err, rows) => {
+            if (err) {
+              throw err;
+            }
+            console.log(rows);
           }
-          console.log(rows);
-        }
-      );
-    });
+        );
+      });
+    resolve();
+  });
 }
 
 function addRole() {
-  db.query(
-    `SELECT id, title, salary, department_id FROM roles`,
-    (err, rows) => {
-      if (err) {
-        throw err;
-      }
-      let departmentChoices = rows.map((department) => {
-        return {
-          name: department.department_name,
-          value: department.id,
-        };
-      });
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "title",
-            message: "What is the name of the role?",
-          },
-          {
-            type: "input",
-            name: "salary",
-            message: "What is the salary for this role?",
-          },
-          {
-            type: "list",
-            name: "department_id",
-            message: "Which department does this role belong to?",
-            choices: departmentChoices,
-          },
-        ])
-        .then((answers) => {
-          db.query(
-            "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)",
-            [answers.title, answers.salary, answers.department_id],
-            (err, rows) => {
-              if (err) {
-                throw err;
-              }
-            }
-          );
+  new Promise((resolve, reject) => {
+    db.query(
+      `SELECT id, title, salary, department_id FROM roles`,
+      (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        let departmentChoices = rows.map((department) => {
+          return {
+            name: department.department_name,
+            value: department.id,
+          };
         });
-    }
-  );
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "title",
+              message: "What is the name of the role?",
+            },
+            {
+              type: "input",
+              name: "salary",
+              message: "What is the salary for this role?",
+            },
+            {
+              type: "list",
+              name: "department_id",
+              message: "Which department does this role belong to?",
+              choices: departmentChoices,
+            },
+          ])
+          .then((answers) => {
+            db.query(
+              "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)",
+              [answers.title, answers.salary, answers.department_id],
+              (err, rows) => {
+                if (err) {
+                  throw err;
+                }
+              }
+            );
+          });
+      }
+    );
+    resolve();
+  });
 }
 
 function addEmployee() {
-  db.query(
-    `SELECT id, first_name, last_name, role_id, manager_id FROM employees`,
-    (err, rows) => {
-      if (err) {
-        throw err;
-      }
-      let roleChoices = rows.map((role) => {
-        return {
-          name: role.role_name,
-          value: role.id,
-        };
-      });
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "first_name",
-            message: "What is the first name of this employee?",
-          },
-          {
-            type: "input",
-            name: "last_name",
-            message: "What is the last name of this employee?",
-          },
-          {
-            type: "list",
-            name: "role_id",
-            message: "Which role does this employee have to?",
-            choices: roleChoices,
-          },
-          {
-            type: "input",
-            name: "manager_id",
-            message: "What's their manager Id?",
-          },
-        ])
-        .then((answers) => {
-          db.query(
-            "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?)",
-            [
-              answers.firstName,
-              answers.last_name,
-              answers.role_id,
-              answers.manager_id,
-            ],
-            (err, rows) => {
-              if (err) {
-                throw err;
-              }
-            }
-          );
+  new Promise((resolve, reject) => {
+    db.query(
+      `SELECT id, first_name, last_name, role_id, manager_id FROM employees`,
+      (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        let roleChoices = rows.map((role) => {
+          return {
+            name: role.role_name,
+            value: role.id,
+          };
         });
-    }
-  );
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "first_name",
+              message: "What is the first name of this employee?",
+            },
+            {
+              type: "input",
+              name: "last_name",
+              message: "What is the last name of this employee?",
+            },
+            {
+              type: "list",
+              name: "role_id",
+              message: "Which role does this employee have to?",
+              choices: roleChoices,
+            },
+            {
+              type: "input",
+              name: "manager_id",
+              message: "What's their manager Id?",
+            },
+          ])
+          .then((answers) => {
+            db.query(
+              "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?)",
+              [
+                answers.firstName,
+                answers.last_name,
+                answers.role_id,
+                answers.manager_id,
+              ],
+              (err, rows) => {
+                if (err) {
+                  throw err;
+                }
+              }
+            );
+          });
+      }
+    );
+    resolve();
+  });
 }
 
 const finished = () => {
